5d153cd31bcd425a3a5cbf262020dcecb7074684
mailtoy <mai_kms11@hotmail.com>
2019-05-01 04:42:30 +0700

    Deleted search.js file in home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
-import Search from './components/home/search';
 import Explore from './Pages/Explore';
 import BasicMap from './components/map/map'
 
